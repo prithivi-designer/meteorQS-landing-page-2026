010e3b220b709395fa1abec1ab0b87d453c70eb3
Nimalraj <nimalraj.panneerselvam@gmail.com>
2025-12-15 22:42:58 +0530

    contentful config updated

--- src/lib/contentful.js
+++ src/lib/contentful.js
@@ -1,20 +1,20 @@
-// import { createClient } from "contentful";
-
-// export const client = createClient({
-//   space: process.env.NEXT_PUBLIC_CONTENTFUL_SPACE_ID,
-//   accessToken: process.env.NEXT_PUBLIC_CONTENTFUL_ACCESS_TOKEN,
-// });
-
 import { createClient } from "contentful";
 
-const space = "0j1vh9j4mlnb";
-const accessToken = "jdVQvfIp3PnCyS6mXyMPLWr68kaZr5oGcTG0IN91AXc";
-
-if (!space || !accessToken) {
-  throw new Error("Contentful environment variables are missing");
-}
-
 export const client = createClient({
-  space,
-  accessToken,
+  space: "0j1vh9j4mlnb",
+  accessToken: "jdVQvfIp3PnCyS6mXyMPLWr68kaZr5oGcTG0IN91AXc",
 });
+
+// import { createClient } from "contentful";
+
+// const space = "0j1vh9j4mlnb";
+// const accessToken = "jdVQvfIp3PnCyS6mXyMPLWr68kaZr5oGcTG0IN91AXc";
+
+// if (!space || !accessToken) {
+//   throw new Error("Contentful environment variables are missing");
+// }
+
+// export const client = createClient({
+//   space,
+//   accessToken,
+// });
