8a704ad0a3d4d63562f13a2fc897603000f10066
Nimalraj <nimalraj.panneerselvam@gmail.com>
2025-12-15 21:15:44 +0530

    Feedback corrections updated

--- src/lib/contentfull.js
+++ src/lib/contentfull.js
@@ -1,6 +1,20 @@
+// import { createClient } from "contentful";
+
+// export const client = createClient({
+//   space: process.env.NEXT_PUBLIC_CONTENTFUL_SPACE_ID,
+//   accessToken: process.env.NEXT_PUBLIC_CONTENTFUL_ACCESS_TOKEN,
+// });
+
 import { createClient } from "contentful";
 
+const space = process.env.CONTENTFUL_SPACE_ID;
+const accessToken = process.env.CONTENTFUL_ACCESS_TOKEN;
+
+if (!space || !accessToken) {
+  throw new Error("Contentful environment variables are missing");
+}
+
 export const client = createClient({
-  space: process.env.NEXT_PUBLIC_CONTENTFUL_SPACE_ID,
-  accessToken: process.env.NEXT_PUBLIC_CONTENTFUL_ACCESS_TOKEN,
+  space,
+  accessToken,
 });
